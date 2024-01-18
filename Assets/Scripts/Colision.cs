a52a10e00c03f14a134711e6ab7a5ba63d41f920
Diana Carolina <dicaro2001@hotmail.com>
2024-01-18 16:34:35 -0500

    Update Colision.cs

--- Assets/Scripts/Colision.cs
+++ Assets/Scripts/Colision.cs
@@ -8,22 +8,28 @@ public class Colision : MonoBehaviour
     public GameObject imagen;
     public TextMeshProUGUI[] dialogo;
     public GameObject[] ListaCamaras;
-  
+
     public MeshRenderer myMeshRenderer;
     public Movimiento myMovimiento;
-    public GameObject Jugador;
+    public GameObject Jugador1;
+    public GameObject Jugador2;
 
-   
+
+
 
 
     private void Start()
-    { 
-       
-        ListaCamaras[0].gameObject.SetActive(true);
-        ListaCamaras[1].gameObject.SetActive(true);
-        ListaCamaras[2].gameObject.SetActive(false);
-        ListaCamaras[3].gameObject.SetActive(false);
-        Jugador = Jugador.gameObject;
+    {
+
+
+        ListaCamaras[0].gameObject.SetActive(true);// camaraplayer1
+        ListaCamaras[1].gameObject.SetActive(true);//camaraplayer2
+        ListaCamaras[2].gameObject.SetActive(false);//canonplayer1
+        ListaCamaras[3].gameObject.SetActive(false);//canonplayer2
+        ListaCamaras[4].gameObject.SetActive(false);//power
+
+        Jugador1 = Jugador1.gameObject;
+        Jugador2 = Jugador2.gameObject;
         desactivar();
 
 
@@ -33,13 +39,35 @@ public class Colision : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
 
-        
+
         if (collision.gameObject.CompareTag("deteccion"))
         {
 
             myMeshRenderer.enabled = false;
 
-            Jugador.transform.position = new Vector3(-10.0500002f, 3.03999996f, 11.3400002f);
+
+            gameObject.transform.position = new Vector3(-9.76000023f, 3.0539999f, 8.14000034f);
+
+            myMeshRenderer.enabled = true;
+
+
+            ListaCamaras[4].gameObject.SetActive(true);
+            ListaCamaras[0].gameObject.SetActive(false);
+
+            //  myMovimiento.enabled = false;
+
+
+
+            // activar();
+
+        }
+        if (collision.gameObject.CompareTag("Jugador"))
+        {
+
+            myMeshRenderer.enabled = false;
+
+            gameObject.transform.position = new Vector3(-10.0500002f, 3.03999996f, 11.3400002f);
+ //           Jugador2.transform.position = new Vector3(-9.68999958f, 3.0539999f, 5.03100014f);
 
             myMeshRenderer.enabled = true;
 
@@ -54,6 +82,28 @@ public class Colision : MonoBehaviour
             activar();
 
         }
+        if (collision.gameObject.CompareTag("Jugador2"))
+        {
+
+            myMeshRenderer.enabled = false;
+
+
+            gameObject.transform.position = new Vector3(-9.68999958f, 3.0539999f, 5.03100014f);
+
+            myMeshRenderer.enabled = true;
+
+
+            ListaCamaras[2].gameObject.SetActive(true);
+            ListaCamaras[0].gameObject.SetActive(false);
+
+            myMovimiento.enabled = false;
+
+
+
+            activar();
+
+        }
+
         if (collision.gameObject.CompareTag("pendulo"))
         {
             ListaCamaras[1].gameObject.SetActive(false);
